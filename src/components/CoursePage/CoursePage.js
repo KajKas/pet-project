3f96cdcd4498b8b3f1df1bd53dfc478944c6424a
kajetan.kasperuk <kajetan.kasperuk@neoteric.eu>
2019-07-08 10:27:41 +0200

    refactor redux to use mapStateToProps method

--- src/components/CoursePage/CoursePage.js
+++ src/components/CoursePage/CoursePage.js
@@ -1,22 +1,28 @@
-import React from 'react'
-import store from '../../redux/store'
+import React, { Component } from 'react'
+import { connect } from "react-redux"
 
-const CoursePage = () => {
-  const state = store.getState()
-  const { courses, activeCourse } = state
-
-  return (
-    <div>
-      <h1>course name</h1>
-      <iframe
-        title={courses[activeCourse].name} // TODO: change to getting from the address, otherwise fails after reload
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        src={courses[activeCourse].address}
-        frameBorder="0"
-        allowFullScreen
-      />
-    </div>
-  )
+class CoursePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>course name</h1>
+        <iframe
+          title={this.props.courses[this.props.activeCourse].name} // TODO: change to getting from the address, or fail after reload
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          src={this.props.courses[this.props.activeCourse].address}
+          frameBorder="0"
+          allowFullScreen
+        />
+      </div>
+    )
+  }
 }
 
-export default CoursePage
+const mapStateToProps = state => {
+  return {
+    courses: state.courses,
+    activeCourse: state.activeCourse
+  }
+}
+
+export default connect(mapStateToProps)(CoursePage)
--- src/components/MyCoursesPage/MyCoursesPage.js
+++ src/components/MyCoursesPage/MyCoursesPage.js
@@ -1,31 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import _ from 'lodash'
+import { connect } from "react-redux"
 import { Link } from "react-router-dom";
 import store from '../../redux/store'
 import { setActiveCourse } from "../../redux/actions"
 import placeholder from "../images/placeholder.jpg";
 
-const MyCoursesPage = () => {
-  const courses = _.values(store.getState().courses)
-  const handleCourseClick = id => {
-    store.dispatch(setActiveCourse(id))
-  }
+class MyCoursesPage extends Component {
+  render() {
+    const courses = _.values(this.props.courses)
+    const handleCourseClick = id => {
+      store.dispatch(setActiveCourse(id))
+    }
 
-  return (
-    <div>
-      <h1>List of your courses:</h1>
-      {
-        courses.map(course => (
-          <div onClick={handleCourseClick.bind(null, course.id)} key={course.id}>
-            <Link  to={'/courses/' + course.id}>
-              <img src={placeholder} alt="link-to-video" />
-              <h3>{course.name}</h3>
-            </Link>
-          </div>
-        ))
-      }
-    </div>
-  )
+    return (
+      <div>
+        <h1>List of your courses:</h1>
+        {
+          courses.map(course => (
+            <div onClick={handleCourseClick.bind(null, course.id)} key={course.id}>
+              <Link  to={'/courses/' + course.id}>
+                <img src={placeholder} alt="link-to-video" />
+                <h3>{course.name}</h3>
+              </Link>
+            </div>
+          ))
+        }
+      </div>
+    )
+  }
 }
 
-export default MyCoursesPage
+const mapStateToProps = state => {
+  return {
+    courses: state.courses
+  }
+}
+
+export default connect(mapStateToProps)(MyCoursesPage)
