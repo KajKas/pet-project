ab8f3e80e5bab6654bad04d468e6915c51d5413c
kajetan.kasperuk <kajetan.kasperuk@neoteric.eu>
2019-07-24 14:07:39 +0200

    Updates

---
+++ precache-manifest.7e7c14ffc41c66680e9cd905228ee4dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8342f850f5f7f003f9a6044b87281f3",
+    "url": "/KajKas/pet-project/index.html"
+  },
+  {
+    "revision": "318a9c3bfe46b293af5e",
+    "url": "/KajKas/pet-project/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7cbd11eb64b8b5e093df",
+    "url": "/KajKas/pet-project/static/js/2.591f82ab.chunk.js"
+  },
+  {
+    "revision": "318a9c3bfe46b293af5e",
+    "url": "/KajKas/pet-project/static/js/main.720aa951.chunk.js"
+  },
+  {
+    "revision": "a3402d90775d4cc9a159",
+    "url": "/KajKas/pet-project/static/js/runtime~main.56dcd045.js"
+  },
+  {
+    "revision": "a494d17ce4c72d962b9178757551266f",
+    "url": "/KajKas/pet-project/static/media/placeholder.a494d17c.jpg"
+  },
+  {
+    "revision": "3fbd74cfd34205f22beaf367bf9d3a9b",
+    "url": "/KajKas/pet-project/static/media/search_icon.3fbd74cf.png"
+  }
+]);
